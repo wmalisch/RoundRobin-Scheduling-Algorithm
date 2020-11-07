9e4dd80983684cdc950ac7c379fe381a1562d668
Will Malisch <wmalisch@uwo.ca>
2020-11-07 15:45:33 -0500

    fundamentals of algorithm in place, need to finsih the rest and add some bug fixes

--- skeleton/scheduling.c
+++ skeleton/scheduling.c
@@ -20,13 +20,43 @@
     rr_result -> order_n: int, representing the size of the `order` array, described above ^
 	rr_result -> turnarounds: 1D int array, containing the turnaround time for each process (i.e. `(rr_result -> turnarounds)[0]` is the turnaround time for p1)
 */
+
+
+
+
 rr_result *rr(int *queue, int np, int tq)
 {
 	rr_result *result = malloc(sizeof(rr_result));
 	result->np = np;
 	result->turnarounds = malloc(sizeof(int) * np);
 
-	// code here to assign values to result->turnarounds, result->order, and result->order_n
+	// Set order_n to np
+	result->order_n = np;
+
+	// Create a copy of the queue to store the REMAINING time needed to complete a process, and store the order in the result object
+	int remainTime[np];
+	result->order = malloc(sizeof(int) * np);
+	for(int count = 0; count < np - 1; count ++){
+		remainTime[count] = queue[count];
+		(result->order)[count] = count + 1;
+	}
+
+	int count, time, remain, tat, wt,marker;
+	count = 0;
+	time = 0;
+	remain = np;
+	tat = 0;
+	wt = 0;
+	marker = 0;
+	while(remain != 0){
+		if(remainTime[count] <= tq && remainTime[count] > 0){
+			remainTime[count] = 0;
+			marker = 1;
+			time += remainTime[count]
+		}
+
+
+	}
 
 	return result;
 }
