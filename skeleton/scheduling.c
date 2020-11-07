6f8aadbf9c8bca7bd2e55f0578a2230550ce7465
Will Malisch <wmalisch@uwo.ca>
2020-11-07 11:22:23 -0500

    Project set up

---
+++ skeleton/scheduling.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include "scheduling.h"
+
+/*
+##  Round-Robin Scheduling Algorithm  ##
+
+	Parameters:-
+
+	queue: 1D int array containing the CPU burst time for every process in a ready queue
+	np: number of processes (length of `queue` array)
+	tq: time quantum for the RR algorithm
+
+
+	Returns:-
+
+	rr_result struct pointer, where
+	rr_result -> order: 1D int array, containing the execution order of processes (i.e. `(rr_result -> order)[0]` is the process number of the first process)
+		** hint: p1 is always the first item in order of selection in round-robin and FCFS
+    rr_result -> order_n: int, representing the size of the `order` array, described above ^
+	rr_result -> turnarounds: 1D int array, containing the turnaround time for each process (i.e. `(rr_result -> turnarounds)[0]` is the turnaround time for p1)
+*/
+rr_result *rr(int *queue, int np, int tq)
+{
+	rr_result *result = malloc(sizeof(rr_result));
+	result->np = np;
+	result->turnarounds = malloc(sizeof(int) * np);
+
+	// code here to assign values to result->turnarounds, result->order, and result->order_n
+
+	return result;
+}
